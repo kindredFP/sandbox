99e5e09251971f3cc967b9b568b0ccdb3a9239ee
Francis <kindredfp@users.noreply.github.com>
2024-07-09 17:40:18 -0400

    Updated file to include a simple js method

---
+++ script.js
@@ -0,0 +1,5 @@
+function changeColor() {
+    const colors = ['red', 'blue', 'green', 'yellow', 'pink', 'purple'];
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    document.body.style.backgroundColor = randomColor;
+}
