1c29c57229ec018dc0de93c861971396c83efa6c
Francis <kindredfp@users.noreply.github.com>
2024-07-12 18:09:23 -0400

    Fixed lint issues with cypress

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,43 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-
+import pluginCypress from "eslint-plugin-cypress";
 
 export default [
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  {
+    ignores: [
+      "cypress.config.js"
+    ]
+  },
+  { 
+    languageOptions: { 
+      globals: {
+        ...globals.browser, 
+        ...globals.node,
+        ...globals.cypress  // Add Cypress globals
+      }
+    }
+  },
   pluginJs.configs.recommended,
+  {
+    plugins: {
+      cypress: pluginCypress
+    },
+    rules: {
+      'no-unused-vars': 'off',  // Turn off no-unused-vars rule
+    }
+  },
+  {
+    files: ["cypress/**/*.js", "cypress/**/*.ts"],
+    languageOptions: {
+      globals: {
+        describe: "readonly",
+        it: "readonly",
+        cy: "readonly",
+        beforeEach: "readonly"
+      }
+    },
+    rules: {
+      "no-undef": "off"
+    }
+  }
 ];
